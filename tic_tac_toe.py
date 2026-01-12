a8cd084b85b0a746f4f0cbc9b9c304314cd95071
Braxton <braxton.d.lowe@gmail.com>
2026-01-11 23:23:34 -0800

    added functions player_move() and valid_move()

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,2 +1,39 @@
 # game functionlity
 
+# use curses, termbox, or rich for terminal idea. Leanning more towards rich
+
+# use pytest for testing
+
+board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+]
+
+def player_move(board, player):
+    while True: 
+        xCord = int(input("Input x coordinate Move: "))
+        yCord = int(input("Input y coordinate Move: "))
+        if valid_move(board, xCord, yCord) == True:
+            board[yCord][xCord] = player
+            return
+        else:
+            print("Invalid Move, Space Occupied")
+
+def valid_move(board, xCord, yCord):
+    if board[yCord][xCord] == " ":
+        return True
+    else: 
+        return False
+
+# valid_move(board, 0, 0)
+# board[0][0] = "O"
+# valid_move(board, 0, 0)
+
+while True:
+    print(board)
+    player_move(board, "X")
+    print(board)
+    player_move(board, "O")
+
+
