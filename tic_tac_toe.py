4364fd3f409c14990f65f823e5d1d12a3126c185
Braxton <braxton.d.lowe@gmail.com>
2026-02-06 14:56:16 -0800

    added check_board and print_board

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -26,14 +26,37 @@ def valid_move(board, xCord, yCord):
     else: 
         return False
 
-# valid_move(board, 0, 0)
-# board[0][0] = "O"
-# valid_move(board, 0, 0)
+def check_board(board):
+    # check rows
+    for row in board:
+        if row[0] != " " and row[0] == row[1] == row[2]:
+            return row[0]
+
+    # check columns
+    for col in range(3):
+        if board[0][col] != " " and board[0][col] == board[1][col] == board[2][col]:
+            return board[0][col]
+    
+    # check diagonals
+    if board[0][0] != " " and board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+
+    if board[0][2] != " " and board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+
+    # no winner
+    return None
+
+def print_board(board):
+    for i, row in enumerate(board):
+        print(" | ".join(row))
+        if i < 2:
+            print("--+---+--")
 
 while True:
-    print(board)
+    print_board(board)
     player_move(board, "X")
-    print(board)
+    print_board(board)
     player_move(board, "O")
 
 
